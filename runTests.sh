536fe7a50935f50f0cdbfba527aebf133c74ec45
Massimo Galbusera <max.galbusera@gmail.com>
2015-03-14 20:55:28 +0100

    run mockserver and selenium from runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,17 +1,33 @@
-#!/bin/sh
-
-# Exit the script as soon as something fails
-set -e
+#!/usr/bin/env bash
 
 #Lint the coffee files
 coffeelint -r coffee/
+status_coffeelint=$?
 
-(
-	cd tests
+#Run background jobs
+java -jar tests/mockserver-netty-3.9.1-jar-with-dependencies.jar -serverPort 9999 > /dev/null 2>&1 &
+java -jar tests/selenium-server-standalone-2.44.0.jar > /dev/null 2>&1 &
+sleep 3
 
-	#Set the urls of the mockserver
-	node setMocks.js
+cd tests
 
-	#Run tests
-	nightwatch --test
-)
+#Set the urls of the mockserver
+node setMocks.js
+
+#Run tests
+nightwatch --test
+status_nightwatch=$?
+
+cd ..
+
+#Kill background jobs
+killall java
+wait
+
+if [[ ${status_coffeelint} != 0 ]] ; then
+    exit ${status_coffeelint}
+fi
+
+if [[ ${status_nightwatch} != 0 ]] ; then
+    exit ${status_nightwatch}
+fi
