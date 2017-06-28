db354968800c9f41771b7ee594968eab22e5be66
Massimo Galbusera <max.galbusera@gmail.com>
2017-06-28 16:25:00 +0200

    fix: avoid killing all java processes + trap ctrl+c and kill mockserver + run nightwatch locally (remove requirements of global nightwatch)

--- runTests.sh
+++ runTests.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
 
-if ! which nightwatch ; then
-	echo "Nightwatch not installed globally. This is necessary for running tests"
-	echo "npm install nightwatch -g"
+if [ ! -f ./node_modules/.bin/nightwatch ]; then
+	echo "Nightwatch not installed. This is necessary for running tests"
+	echo "Run `npm install` to install it locally"
 	exit 1
 fi
 
 NIGHTWATCH_ENV=${NIGHTWATCH_ENV:-default}
 
 #Run background jobs
+echo "Starting mockserver..."
 java -Droot.logLevel=OFF -Dmockserver.logLevel=OFF -jar tests/mockserver-netty-3.9.17-jar-with-dependencies.jar -serverPort 9999 > /dev/null 2>&1 &
+MOCKSERVER_PID=$!
 sleep 3
+
+trap ctrl_c INT
+function ctrl_c() {
+	echo "Killing mockserver..."
+	kill $MOCKSERVER_PID > /dev/null 2>&1
+	wait
+
+	exit 1
+}
 
 cd tests
 
 #Set the urls of the mockserver
+echo "Creating mocks..."
 node setMocks.js
 
 #Run tests
-nightwatch --test --env "$NIGHTWATCH_ENV"
+../node_modules/.bin/nightwatch --test --env "$NIGHTWATCH_ENV"
 status_nightwatch=$?
 
 cd ..
 
-#Kill background jobs
-killall java
+echo "Killing mockserver..."
+kill $MOCKSERVER_PID > /dev/null 2>&1
 wait
 
 if [[ ${status_nightwatch} != 0 ]] ; then
